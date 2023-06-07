8701ec534fb8da4a9fc693ca5d2a9d064982a58a
kibria5 <106745675+kibria5@users.noreply.github.com>
2023-06-07 17:17:19 +0000

    ginkgo: Add speed preopt for Trebuchet/Settings/SystemUI

--- device.mk
+++ device.mk
@@ -116,6 +116,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    TrebuchetQuickStep \
+    Settings \
+    SystemUI
+
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
